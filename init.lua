7cdbdc2c41291be5f1769dcd43916cb14c00f61d
Oussama Ezzaou <oezzaou@c2r1p9.1337.ma>
2024-12-02 20:17:09 +0100

    update config

--- init.lua
+++ init.lua
@@ -27,7 +27,7 @@ vim.o.autoindent = true          -- copy indent from current line
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clon", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
